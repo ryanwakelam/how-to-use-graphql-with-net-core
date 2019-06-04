148f1dd8df7fd4610387f727d7cbc88f0db93d3f
ryan.wakelam <ryan.wakelan@aucerna.com>
2019-06-04 14:44:42 -0600

    Create GraphQL endpoint

--- DevPlanning.WebServer/Startup.cs
+++ DevPlanning.WebServer/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using DevelopmentPlanning.Schema;
+using GraphQL.Server;
 
 namespace DevPlanning.WebServer
 {
@@ -16,6 +18,10 @@ namespace DevPlanning.WebServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<DevelopmentPlanningSchema>();
+
+            services.AddGraphQL(options => { options.ExposeExceptions = true; });
+        
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "client-app/build"; });
         }
 
@@ -27,6 +33,8 @@ namespace DevPlanning.WebServer
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseGraphQL<DevelopmentPlanningSchema>("/graphql");
+            
             app.UseSpa(spa =>
             {
                 spa.Options.SourcePath = "client-app";

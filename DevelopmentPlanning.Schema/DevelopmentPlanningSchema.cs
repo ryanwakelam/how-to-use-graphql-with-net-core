4d5bf700308716b90f0c1ec7c49e746fa57c9a8b
ryan.wakelam <ryan.wakelan@aucerna.com>
2019-06-07 14:20:19 -0600

    Add a parameterized query and a new field for deliverable description

--- DevelopmentPlanning.Schema/DevelopmentPlanningSchema.cs
+++ DevelopmentPlanning.Schema/DevelopmentPlanningSchema.cs
@@ -1,4 +1,5 @@
 ﻿using GraphQL.Types;
+using System.Linq;
 
 namespace DevelopmentPlanning.Schema
 {
@@ -14,12 +15,21 @@ namespace DevelopmentPlanning.Schema
     {
         public DevelopmentPlanningQuery()
         {
-            Field<ListGraphType<DeliverableType>>("deliverables", resolve: context => new []
+            var deliverables = new []
             {
-                new Deliverable{Id = "1", Name = "Daily data enhancements", PBI = "VN-2739", Status = "Completed"},
-                new Deliverable{Id = "2", Name = "US Reporting", PBI = "VN-2844", Status = "In-Progress"},
-                new Deliverable{Id = "3", Name = "Misc. Bugs / Regression", PBI = "", Status = "Scoping"},
-            });
+                new Deliverable{Id = "1", Name = "Daily data enhancements", PBI = "VN-2739", Status = "Completed", Description = "Long clothes lanyard Plate Fleet barkadeer bucko Shiver me timbers Sea Legs matey piracy clipper. "},
+                new Deliverable{Id = "2", Name = "US Reporting", PBI = "VN-2844", Status = "In-Progress", Description = "Nipperkin lookout aye Pirate Round hogshead mutiny loaded to the gunwalls Jack Tar Jack Ketch gabion."},
+                new Deliverable{Id = "3", Name = "Misc. Bugs / Regression", PBI = "", Status = "Scoping", Description = "Mizzenmast code of conduct bucko Sea Legs loot pirate spirits marooned scourge of the seven seas Arr."},
+            };
+            
+            Field<ListGraphType<DeliverableType>>("deliverables", resolve: context => deliverables);
+            Field<DeliverableType>("deliverable",
+                arguments: new QueryArguments(new QueryArgument<IdGraphType> {Name = "id"}), resolve:
+                context =>
+                {
+                    var id = context.GetArgument<string>("id");
+                    return deliverables.First(x => x.Id == id);
+                });
         }
     }
 
@@ -31,6 +41,7 @@ namespace DevelopmentPlanning.Schema
             Field(o => o.Name);
             Field(o => o.PBI).Name("pbi");
             Field(o => o.Status);
+            Field(o => o.Description);
         }
     }
 
@@ -40,5 +51,6 @@ namespace DevelopmentPlanning.Schema
         public string Name { get; set; }
         public string PBI { get; set; }
         public string Status { get; set; }
+        public string Description { get; set; }
     }
 }
